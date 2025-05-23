01f8d71044e5674bb5dcb9367baa766d8e51ad7d
Joseph Morin <joseph.morin@prestashop.com>
2025-05-23 10:50:06 +0200

    feat: added few plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,25 +2,24 @@ return {
 	-- the colorscheme should be available when starting Neovim
 	{
 		"folke/tokyonight.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		lazy = true, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 	},
 	{
 		"marko-cerovac/material.nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000,
 	},
 	{
 		"nvimdev/zephyr-nvim",
-		lazy = false,
+		lazy = true,
 		priority = 1000,
 	},
 
 	{
 		"uloco/bluloco.nvim",
 		dependencies = { 'rktjmp/lush.nvim' },
-
-		lazy = false,
+		lazy = true,
 		priority = 1000,
 	},
 	{
---
+++ lua/plugins/garbage-day.lua
@@ -0,0 +1,9 @@
+return {
+	"zeioth/garbage-day.nvim",
+	event = "VeryLazy",
+	opts = {
+		grace_period = 15,
+		aggresive_mode = true,
+		notifications = true,
+	}
+}
