dd43dcd09393f580710ea878432c4b4ca2266c2e
Joseph Morin <joseph.morin@prestashop.com>
2025-06-24 09:44:54 +0200

    feat: cleanup legacy config

--- lua/lsp/nvim-cmp.lua
+++ lua/lsp/nvim-cmp.lua
@@ -49,8 +49,6 @@ M.config = function()
 			}),
 		},
 		mapping = cmp.mapping.preset.insert({
-			--["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			--["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-Space>"] = cmp.mapping.complete(),
 			["<C-e>"] = cmp.mapping.abort(),
 			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -63,15 +61,6 @@ M.config = function()
 			{ name = 'luasnip',  keyword_length = 2 },
 		}),
 	})
-
-	--cmp.setup.cmdline(":", {
-	--	mapping = cmp.mapping.preset.cmdline(),
-	--	sources = cmp.config.sources({
-	--		{ name = "path" },
-	--	}, {
-	--		{ name = "cmdline" },
-	--	}),
-	--})
 end
 
 return M
