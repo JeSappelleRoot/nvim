01d43e26c44f031aad4007f280a4fa0273596d9d
jmorin <joseph.morin@prestashop.com>
2025-09-08 07:12:15 +0200

    fix: added wrap and sidescroll options to fix layout

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -1,3 +1,11 @@
 return {
 	"Isrothy/neominimap.nvim",
+	init = function()
+		vim.opt.wrap = false
+		vim.opt.sidescrolloff = 45
+		--	vim.g.neominimap = {
+		--		auto_enable = true,
+		--		layout = "split",
+		--	}
+	end,
 }
