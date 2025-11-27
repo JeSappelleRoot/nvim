8b589a13151e7e228794616358a47e65ee96b52d
scratch <joseph.morin.it@gmail.com>
2025-11-27 10:29:25 +0100

    feat: added go on treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,24 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
-		require("nvim-treesitter.configs").setup {
-			ensure_installed = { "yaml", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "terraform", "hcl", "dockerfile" },
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"yaml",
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+				"terraform",
+				"hcl",
+				"dockerfile",
+				"go",
+			},
 			highlight = {
 				enable = true,
 			},
-		}
+		})
 	end,
 }
