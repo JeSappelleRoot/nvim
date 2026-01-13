591a5f80b7ca07795f915902dc1b5dd67e5c79b9
jmorin <joseph.morin@prestashop.com>
2026-01-13 08:07:12 +0100

    feat: added some tweaks to hardtime plugin

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,6 +2,14 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim" },
 	config = function()
-		require("hardtime").setup({})
+		require("hardtime").setup({
+			restriction_mode = "hint",
+			disabled_keys = {
+				["<Up>"] = false,
+				["<Down>"] = false,
+				["<Left>"] = false,
+				["<Right>"] = false,
+			},
+		})
 	end,
 }
