8398a3a70493bc1fddcdb32fb8eda87dcd21dd7a
Joseph Morin <joseph.morin@agicap.com>
2026-03-11 18:41:50 +0100

    feat: removed virtualtext option from init.lua

--- init.lua
+++ init.lua
@@ -50,11 +50,6 @@ vim.opt.scrolloff = 10 -- Add 10 extra lines before scrolling up and down
 
 vim.opt.laststatus = 3 -- Set laststatus to 3 to get one status bar per window
 
--- Force some NVim diagnostic settings
-vim.diagnostic.config({
-	virtual_text = true, -- Force virtual text to get linter warning
-})
-
 vim.g.vimtex_syntax_conceal_disable = true -- Fix slow scrolling with vimtex plugin
 
 vim.cmd([[autocmd BufRead,BufNewFile */ansible/*.yml, set filetype=yaml.ansible]])
